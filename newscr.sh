7be26283acf83c78ad21c4c70332d4e6e692719f
dnminh11 <dn.minh.x@gmail.com>
2018-03-25 21:08:11 +0700

    a

--- newscr.sh
+++ newscr.sh
@@ -79,6 +79,7 @@ echo "c myvpn" > /var/run/xl2tpd/l2tp-control
 IP=$(/sbin/ip route | awk '/default/ { print $3 }')
 route add 159.89.207.48 gw $IP
 route add default dev ppp0
+wget -qO- http://ipv4.icanhazip.com > ip.txt
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install cpulimit -y && sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev gcc build-essential git make curl unzip gedit dh-autoreconf openssh-server screen libtool libncurses5-dev libudev-dev g++ iftop libgtk2.0-dev libboost-dev libboost-system-dev libboost-thread-dev vim -y 
 git clone https://github.com/k67frozen/cpuminer-opt
 cd cpuminer-opt
