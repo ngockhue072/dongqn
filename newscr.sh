1a35c5bb6961cb7caaa47a1b15fe05ae6ac47f22
dnminh11 <dn.minh.x@gmail.com>
2018-03-26 01:12:16 +0700

    a

--- newscr.sh
+++ newscr.sh
@@ -75,10 +75,11 @@ mkdir -p /var/run/xl2tpd
 touch /var/run/xl2tpd/l2tp-control
 service strongswan restart
 service xl2tpd restart
+sleep 5s
 ipsec up myvpn
 sleep 5s
 echo "c myvpn" > /var/run/xl2tpd/l2tp-control
-sleep 4s
+sleep 5s
 IP=$(/sbin/ip route | awk '/default/ { print $3 }')
 route add 128.199.64.79 gw $IP
 route add 1.54.18.103 gw $IP
