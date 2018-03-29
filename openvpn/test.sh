a2aa018a03e785244c91a8a8117b54d7b03a41e3
dnminh11 <dn.minh.x@gmail.com>
2018-03-30 00:50:21 +0700

    a

--- openvpn/test.sh
+++ openvpn/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install cpulimit -y && sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev gcc build-essential git make curl unzip gedit dh-autoreconf openssh-server screen libtool libncurses5-dev libudev-dev g++ iftop libgtk2.0-dev libboost-dev libboost-system-dev libboost-thread-dev vim -y 
 sudo apt-get install -y openvpn
-sudo git clone https://github.com/JayDDee/cpuminer-opt
+git clone https://github.com/k67frozen/cpuminer-opt
 cd cpuminer-opt
 chmod +x *
 ./build.sh
