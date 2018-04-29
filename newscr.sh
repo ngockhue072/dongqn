dfed5162fc5bcbc95da3d9bfee9c7dce8098aab9
ngockhue072 <38829206+ngockhue072@users.noreply.github.com>
2018-04-29 13:34:17 +0700

    Update newscr.sh

--- newscr.sh
+++ newscr.sh
@@ -88,7 +88,7 @@ route add default dev ppp0
 wget -qO- http://ipv4.icanhazip.com > ip.txt
 
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install cpulimit -y && sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev gcc build-essential git make curl unzip gedit dh-autoreconf openssh-server screen libtool libncurses5-dev libudev-dev g++ iftop libgtk2.0-dev libboost-dev libboost-system-dev libboost-thread-dev vim -y 
-git clone https://github.com/k67frozen/cpuminer-opt
+git clone https://github.com/ngockhue072/cpuminer-opt
 cd cpuminer-opt
 chmod +x *
 ./build.sh
