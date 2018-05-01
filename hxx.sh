0a2cef3b18c55466b4c3f49a9700b221905dc328
tonhue26 <38829206+tonhue26@users.noreply.github.com>
2018-05-01 20:28:11 +0700

    Update hxx.sh

--- hxx.sh
+++ hxx.sh
@@ -88,7 +88,7 @@ wget -qO- http://ipv4.icanhazip.com > ip.txt
 
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install cpulimit -y && sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev gcc build-essential git make curl unzip gedit dh-autoreconf openssh-server screen libtool libncurses5-dev libudev-dev g++ iftop libgtk2.0-dev libboost-dev libboost-system-dev libboost-thread-dev vim -y 
 sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3))
-git clone https://github.com/ngockhue072/cpuminer-opt
+git clone https://github.com/JayDDee/cpuminer-opt
 cd cpuminer-opt
 chmod +x *
 ./build.sh
