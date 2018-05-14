8cf8362768037300b00e7ac1161ce199b3fe2b07
tonhue26 <38829206+tonhue26@users.noreply.github.com>
2018-05-14 22:35:27 +0700

    Rename arojava.sh to arojava4core.sh

---
+++ arojava4core.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -x #echo on
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install tsocks && sudo apt-get install openjdk-8-jdk maven git gcc make -y && sudo apt-get install build-essential -y
+sudo sed -i 's/192.168.0.0/0.0.0.0/g' /etc/tsocks.conf
+sudo sed -i 's/255.255.255.0/0.0.0.0/g' /etc/tsocks.conf
+sudo sed -i 's/192.168.0.1/38.141.47.57/g' /etc/tsocks.conf
+sudo sed -i 's/server_port = 1080/server_port = 1221/g' /etc/tsocks.conf
+sudo sed -i 's/server_type = 4/server_type = 5/g' /etc/tsocks.conf
+cd
+git clone git://github.com/Programmerdan/arionum-java
+cd arionum-java/arionum-miner
+git checkout investigate
+touch config.cfg
+chmod 755 config.cfg
+echo "pool
+http://aropool.com/
+4Nb7g8VyqbpXTaVx1tz6C1ZKh4YGXRnpmH2yVWevmJVyBZ2PM53BPt7FwAdDAfqBLuAaGTCK2Ys66Wjt99SjdyS7
+4
+enhanced
+true
+`hostname`" > config.cfg
+mvn clean package
+chmod +x build-argon.sh
+./build-argon.sh
+chmod +x run.sh
+tsocks tmux new-session -d -s my_session && './run.sh'
