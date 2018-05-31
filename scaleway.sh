f47898f8ca7069b32997e1f564ef57d492461eb0
tonhue26 <38829206+tonhue26@users.noreply.github.com>
2018-05-31 23:00:04 +0700

    Update scaleway.sh

--- scaleway.sh
+++ scaleway.sh
@@ -91,5 +91,4 @@ git clone https://github.com/tonhue26/ariocppminer.git
 cd ariocppminer
 mv ariocppminer baokim
 chmod 0777 baokim
-screen -S run
 cpulimit --exe baokim --limit 160 -b && screen -S run ./baokim
